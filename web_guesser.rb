4a596d9237c2c3b85c499d3f2bdd94eaae0b90f9
DanHyde88 <hyde.dt@gmail.com>
2016-10-19 10:37:09 +0100

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
-get '/' do
-    "Hello, world!"
+random_number = rand(100)
+
+get '/' do    
+    %{<h1>The secret number is #{random_number}</h1>
+    <p>But what about the rest of history?</p>
+    <ul><li>one</li><li>two</li><li>three</li></ul>}
 end
