4d5177e213f4da1152d8143f6518ebb58cf9898f
DanHyde88 <hyde.dt@gmail.com>
2016-10-19 11:37:30 +0100

    I2: uses an ERB template and HTML

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,7 +4,5 @@ require 'sinatra/reloader'
 random_number = rand(100)
 
 get '/' do    
-    %{<h1>The secret number is #{random_number}</h1>
-    <p>But what about the rest of history?</p>
-    <ul><li>one</li><li>two</li><li>three</li></ul>}
+    erb :index, :locals => {:random_number => random_number}
 end
